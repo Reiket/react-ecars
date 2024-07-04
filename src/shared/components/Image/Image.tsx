2a82e6d101c06f50b883366e3dc0f4a51ef3c3e2
Reiket <olinyko2607@gmail.com>
2024-07-04 17:12:00 +0300

    T2-catalog-card: do all functional catalog component and added prettier

--- src/shared/components/Image/Image.tsx
+++ src/shared/components/Image/Image.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {cn} from "../../utils";
-import {ImageProps} from "./types/image.types";
+import React from "react";
+import { cn } from "../../utils";
+import { ImageProps } from "./types/image.types";
 
-const Image: React.FC<ImageProps> = ({name, url, alt, classnames, children}) => {
-    return <div className={cn(name ? `${name}__image` : "image")}>
-        <img className={cn(classnames)} src={url} alt={alt}/>
-        {children}
+const Image: React.FC<ImageProps> = ({
+  name,
+  url,
+  alt,
+  classnames,
+  children,
+}) => {
+  return (
+    <div className={cn(name ? `${name}__image` : "image")}>
+      <img className={cn(classnames)} src={url} alt={alt} />
+      {children}
     </div>
+  );
 };
 
 export default Image;
--- src/shared/components/buttons/FavButton/FavButton.tsx
+++ src/shared/components/buttons/FavButton/FavButton.tsx
@@ -1,24 +1,32 @@
-import React, {FC} from 'react';
-import {checkInFavorites} from "../../../utils";
-import {AiFillHeart, AiOutlineHeart} from "react-icons/ai";
+import React, { FC } from "react";
+import { checkInFavorites } from "../../../utils";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import {
-    selectFavorites,
-    selectIsLoading
+  selectFavorites,
+  selectIsLoading,
 } from "../../../../components/Pages/components/Favorites/store/selector/favorites-selector";
-import {useFavorites} from "../../../../components/Pages/shared/hooks/useFavorites";
-import {FavoriteButton} from "./types/fav.btn.types";
-import {useAppSelector} from "../../../../app/store/hooks";
+import { useFavorites } from "../../../../components/Pages/shared/hooks/useFavorites";
+import { FavoriteButton } from "./types/fav.btn.types";
+import { useAppSelector } from "../../../../app/store/hooks";
 
-const FavButton: FC<FavoriteButton> = ({text, item, classnames = "card-fav-button"}) => {
-    const isLoading = useAppSelector(selectIsLoading)
-    const favorites = useAppSelector(selectFavorites)
-    const {onClickToFavorite} = useFavorites();
-    return <button disabled={isLoading}
-                   onClick={() => onClickToFavorite(item)}
-                   className={classnames}>
-        {checkInFavorites(item.id, favorites) || isLoading ? <AiFillHeart /> : <AiOutlineHeart />}
-        {text}
+const FavButton: FC<FavoriteButton> = ({ text, item }) => {
+  const isLoading = useAppSelector(selectIsLoading);
+  const favorites = useAppSelector(selectFavorites);
+  const { onClickToFavorite } = useFavorites();
+  return (
+    <button
+      disabled={isLoading}
+      onClick={() => onClickToFavorite(item)}
+      className={"card-fav-button"}
+    >
+      {checkInFavorites(item.id, favorites) || isLoading ? (
+        <AiFillHeart />
+      ) : (
+        <AiOutlineHeart />
+      )}
+      {text}
     </button>
+  );
 };
 
 export default FavButton;
